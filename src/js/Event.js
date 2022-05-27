1ee2b79d69b350f7a0b0d11757ea6e08aef12cad
ikoroleva <prsml.ellisson@gmail.com>
2022-05-27 10:27:03 +0200

    some layout

---
+++ src/js/Event.js
@@ -0,0 +1,53 @@
+export class Event {
+    constructor(header) {
+        this.header = header;
+        this.element = document.createElement('div');
+        this.element.classList.add('event');
+        this.update();
+    }
+
+    async sendRegistration() {
+        const res = await fetch('https://test-api.codingbootcamp.cz/api/9c68c1d0/ratings', {
+            "method": "POST",
+            "body": JSON.stringify({
+                "rating_subject": this.header,
+                "rating_value": this.value
+            }),
+            "headers": {
+                'Content-Type': 'application/json'
+            }
+        });
+        const response = await res.json();
+        console.log(response);
+    }
+
+    setRating(stars, rating) {
+
+        this.element.querySelector('.rating__value').textContent = rating;
+
+        for (let i = 0; i < stars.length; i++) {
+            if (i < rating) {
+                stars[i].classList.add('rating__star--on');
+            } else {
+                stars[i].classList.remove('rating__star--on');
+            }
+        }
+    }
+
+    update() {
+
+        this.element.innerHTML = `
+        <h1 class="event_name"></h1>
+        <div class="event_description">0</div>
+        <button class = "event_more_button">More info...</button>
+        `
+
+        this.element.querySelector('.event__header').textContent = this.header;
+
+
+        const eventMoreInfo = this.element.querySelector('.event_more_button');
+        textRatingButton.addEventListener('click', async () => {
+            //await this.sendRating();
+        });
+    }
+}
