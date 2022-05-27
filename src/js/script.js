0f1a6e9bb7588eb771cb10b15561e8c62ffce893
harisrahan9249 <harisrahman9249@gmail.com>
2022-05-27 10:58:53 +0200

    firstpull

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,46 @@
 console.log('Hello');
+const btn =  document.querySelector(".R_btn")
+console.log(btn)
+class Person {
+    constructor(firstName, lastName, email,phonenumber,age) {
+        this.firstName = firstName;
+        this.email = email;
+        this.lastName = lastName;
+        this.phonenumber=phonenumber;
+        this.age = age;
+    }
+ 
+    getFullName() {
+        return `${this.firstName} . ${this.lastName}`;
+    }
+    getemail(){
+        return `${this.email}`
+    }
+    getphonenumber(){
+        return `${this.phonenumber}`
+    }
+ 
+    isAdult() {
+        return this.age >= 18;
+    }
+ 
+    sayHello() {
+        console.log(this.isAdult() ? "I'm old enough to participate");
+    }
+};
+ 
+const simpsons = [
+    new Person('', '', '', ),
+    new Person('', '', '', ),
+    new Person('', '', '', ),
+    new Person('', '', '', ),
+    new Person('', '', '', ),
+];
+ 
+simpsons.forEach((person) => {
+    console.log(person.getFullName());
+});
+ 
+let adults = simpsons.filter(person => person.isAdult());
+console.log(adults);
+addEventListener('click',myfunction)
